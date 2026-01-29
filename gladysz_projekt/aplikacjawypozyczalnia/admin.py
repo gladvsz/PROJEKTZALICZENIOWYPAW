fd7533c20f1363b89bab71867ecf9401f6fa616a
gladvsz <tgladysz1@st.swps.edu.pl>
2026-01-29 21:22:59 +0100

    dodane klasy admin

--- gladysz_projekt/aplikacjawypozyczalnia/admin.py
+++ gladysz_projekt/aplikacjawypozyczalnia/admin.py
@@ -3,12 +3,25 @@ from django.contrib import admin
 
 from .models import Samochod, Marka, ModelAuta, Klient, Wypozyczenie
 
-admin.site.register(Samochod)
+class SamochodAdmin(admin.ModelAdmin):
+    list_display = ['marka', 'model', 'rejestracja', 'rok_produkcji']
+    list_filter = ['marka']
+
+admin.site.register(Samochod, SamochodAdmin)
+
+class WypozyczenieAdmin(admin.ModelAdmin):
+    list_display = ['klient', 'auto', 'data_wypo', 'data_zwrotu']
+
+admin.site.register(Wypozyczenie, WypozyczenieAdmin)
+
+class KlientAdmin(admin.ModelAdmin):
+    list_display = ['imie', 'nazwisko', 'email']
+
+admin.site.register(Klient, KlientAdmin)
+
+    
 admin.site.register(Marka)
 admin.site.register(ModelAuta)
-admin.site.register(Klient)
-admin.site.register(Wypozyczenie)
-
 
 
 # Register your models here.
